1c97edc0b63e15aecb3055a0ae0d0b2c4d5200ef
aldhix <aldhi.x@gmail.com>
2021-05-25 01:35:59 +0700

    page-extra

--- src/views/master/page-extra.blade.php
+++ src/views/master/page-extra.blade.php
@@ -24,11 +24,11 @@
 @yield('content')
 
 <!-- jQuery -->
-<script src="{{url('AdminLTE/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{url('adminlte/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{url('AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{url('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{url('AdminLTE/dist/js/adminlte.min.js')}}"></script>
+<script src="{{url('adminlte/dist/js/adminlte.min.js')}}"></script>
 @stack('js')
 </body>
 </html>
